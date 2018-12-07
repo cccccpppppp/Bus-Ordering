f3f5cfcd51003b116ece0116e6e3727bab2fce62
WT <503369325@qq.com>
2018-12-07 20:25:03 +0800

    更改配置文件

--- app.js
+++ app.js
@@ -1,40 +1,51 @@
 //app.js
 App({
   onLaunch: function() {
-    // 展示本地存储能力
-    // var logs = wx.getStorageSync('logs') || []
-    // logs.unshift(Date.now())
-    // wx.setStorageSync('logs', logs)
-
-    // // 登录
-    // wx.login({
-    //   success: res => {
-    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
-    //   }
-    // })
-    // // 获取用户信息
-    // wx.getSetting({
-    //   success: res => {
-    //     if (res.authSetting['scope.userInfo']) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-    //       wx.getUserInfo({
-    //         success: res => {
-    //           // 可以将 res 发送给后台解码出 unionId
-    //           this.globalData.userInfo = res.userInfo
-
-    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //           // 所以此处加入 callback 以防止这种情况
-    //           if (this.userInfoReadyCallback) {
-    //             this.userInfoReadyCallback(res)
-    //           }
-    //         }
-    //       })
-    //     }
-    //   }
-    // })
+    
   },
   globalData: {
     userInfo: null,
     host: "http://47.107.172.101/"
+  },
+
+  //定义查看订车订单状态的方法
+  applyCarLately: function () {
+    wx.showLoading();
+    wx.request({
+      url: host + 'miniprogram/Predetermine/applyCarLately',
+      data: {
+        sessionid: wx.getStorageSync("sessionid")
+      },
+      method: 'GET',
+      success: function (res) {
+        wx.hideLoading();
+        var data = res.data.data;
+        var msg = res.data.msg;
+        var dic = {
+          '已提交': './wait4Verifying/wait4Verifying',
+          '审核通过': './success/success',
+          '审核未通过': './faild/faild',
+          '完成': ''
+        }
+        if (data === null) {
+          wx.showToast({
+            title: msg,
+            icon: 'none'
+          })
+        } else {
+          wx.redirectTo({
+            url: dic[data.status],
+          })
+        }
+      },
+      fail: function (res) {
+        wx.hideLoading();
+        wx.showToast({
+          title: '网络异常',
+          icon: 'none'
+        })
+      },
+      complete: function (res) { },
+    })
   },
 })
