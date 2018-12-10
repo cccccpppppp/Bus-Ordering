c3028478726bb298fe19c11718a26e8eb9e4e7f7
韦韬 <503369325@qq.com>
2018-12-11 01:50:48 +0800

    新增出发地信息用地图选取功能

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,48 +1,33 @@
-var amapFile = require('../../libs/amap-wx.js');
-var config = require('../../libs/config.js');
+//index.js
+//获取应用实例
+const app = getApp()
 
 Page({
   data: {
-    markers: [],
+    name: '',
+    address: '',
     latitude: '',
-    longitude: '',
-    textData: {}
+    longitude: ''
   },
+
   onLoad: function () {
-    var that = this;
-    var key = config.Config.key;
-    var myAmapFun = new amapFile.AMapWX({ key: key });
-    myAmapFun.getRegeo({
-      iconPath: "../../img/marker.png",
-      iconWidth: 22,
-      iconHeight: 32,
-      success: function (data) {
-        var marker = [{
-          id: data[0].id,
-          latitude: data[0].latitude,
-          longitude: data[0].longitude,
-          iconPath: data[0].iconPath,
-          width: data[0].width,
-          height: data[0].height
-        }]
-        that.setData({
-          markers: marker
-        });
-        that.setData({
-          latitude: data[0].latitude
-        });
-        that.setData({
-          longitude: data[0].longitude
-        });
-        that.setData({
-          textData: {
-            name: data[0].name,
-            desc: data[0].desc
-          }
+
+  },
+
+  getLocation:function(){
+    var _this=this;
+    wx.chooseLocation({
+      success: function (res) {
+        var name = res.name
+        var address = res.address
+        var latitude = res.latitude
+        var longitude = res.longitude
+        _this.setData({
+          name: name,
+          address: address,
+          latitude: latitude,
+          longitude: longitude
         })
-      },
-      fail: function (info) {
-        wx.showModal({title:info.errMsg})
       }
     })
   }
