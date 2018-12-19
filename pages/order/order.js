4664d77bf63645b2e72c9f21eb597c6deff74c4d
liuxin <1056498425@qq.com>
2018-12-19 21:36:30 +0800

    新增司机确认完成订单

--- pages/order/order.js
+++ pages/order/order.js
@@ -13,14 +13,15 @@ Page({
     ifload: true, //是否加载界面
     hadundata: true, //是否有数据
     hadfinishdata: true,
-    hadnopassdata : true,
+    hadnopassdata: true,
     hadunmore: true, //是否有更多数据
+    type: "",
     hadfinishmore: true,
-    hadnopassmore:true,
+    hadnopassmore: true,
     ifload_more: false,
-    unfinish :[],//未完成订单
-    finish : [],//已完成订单
-    notpass : [], //未通过订单
+    unfinish: [], //未完成订单
+    finish: [], //已完成订单
+    notpass: [], //未通过订单
     tabs: ["未完成订单", "已完成订单"],
     activeIndex: 0,
     sliderOffset: 0,
@@ -35,7 +36,7 @@ Page({
     unpage = 1;
     page = 1;
     nopage = 1;
-    
+
 
     //获取用户类型
     wx.request({
@@ -45,27 +46,32 @@ Page({
       },
       success(res) {
         if (res.data.data.type == "管理人员" || res.data.data.type == "预订人员") {
+          that.getunfinish(1);
           that.setData({
             tabs: ["未完成订单", "已完成订单", "未通过订单"]
           })
+        } else if (res.data.data.type == "司机") {
+          that.getunfinish(4);
+          that.setData({
+            type: "司机"
+          })
         }
+        that.setData({
+          ifload_more: false,
+          ifload: false,
+        })
       }
-    })//获取用户类型
+    }) //获取用户类型
 
-    this.getunfinish();
+
     this.getfinish();
     this.getnopass();
-    this.setData({
-      ifload_more: false,
-      ifload: false,
-    })
-
   },
 
 
 
   //获取未完成订单
-  getunfinish(){
+  getunfinish(status) {
     var that = this;
     //请求
     wx.request({
@@ -74,7 +80,7 @@ Page({
         sessionid: wx.getStorageSync("sessionid"),
         page: unpage,
         number: 10,
-        status: 1
+        status: status
       },
       success(res) {
         that.setData({
@@ -173,38 +179,72 @@ Page({
     })
   },
 
-  onReachBottom()
-  {
+  onReachBottom() {
     var that = this;
     that.setData({
       ifload_more: true
     })
-    
-    if (this.data.activeIndex == 0)
-    {
-      if(that.data.hadunmore == true)
-      {
-        that.getunfinish();
-      } 
-    }
-    else if (this.data.activeIndex == 1)
-    {
+
+    if (this.data.activeIndex == 0) {
+      if (that.data.hadunmore == true) {
+        if (that.data.type == "司机") {
+          that.getunfinish(4);
+        } else {
+          that.getunfinish(1);
+        }
+      }
+    } else if (this.data.activeIndex == 1) {
       if (that.data.hadfinishmore == true) {
         that.getfinish();
       }
-    }
-    else{
-        if (that.data.hadnopassmore == true) {
-          that.getnopass();
-        }
+    } else {
+      if (that.data.hadnopassmore == true) {
+        that.getnopass();
+      }
     }
     that.setData({
-      ifload_more : false
+      ifload_more: false
+    })
+  },
+
+  //点击订单已完成
+  finish(e) {
+    var id = e.target.id;
+    var that = this;
+    unpage = 1;
+    wx.request({
+      url: host + 'miniprogram/Driver/finish',
+      data: {
+        sessionid: wx.getStorageSync("sessionid"),
+        apply_id: id,
+      },
+      success(res)
+      {
+        if (res.data.status == 0)
+        {
+          wx.showToast({
+            title: '已完成该订单',
+            icon: 'success',
+            duration: 2000
+          })
+          that.setData({
+            unfinish : []
+          })
+          that.getunfinish(4)
+        }
+        else{
+          wx.showToast({
+            title: res.data.msg,
+            icon : "none",
+            duration: 2000
+          })
+        }
+      }
     })
   },
 
   //选项卡点击切换函数
-  tabClick: function (e) {
+  tabClick: function(e) {
     this.setData({
       sliderOffset: e.currentTarget.offsetLeft,
       activeIndex: e.currentTarget.id
