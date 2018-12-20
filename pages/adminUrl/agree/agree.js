597170d6c9be30fcde9f2c906808d16e34e7927a
liuxin <1056498425@qq.com>
2018-12-21 02:29:30 +0800

    更新选择司机功能

--- pages/adminUrl/agree/agree.js
+++ pages/adminUrl/agree/agree.js
@@ -1,5 +1,6 @@
 const app = getApp();
 var host = app.globalData.host;
+var itemList = [];
 Page({
 
   /**
@@ -7,104 +8,78 @@ Page({
    */
   data: {
     id: "1",
-    drive :[],
-    is_can_comment : 1,
-    checkboxItems: [
-      { name: '', value: '0',phone:"", },
-      { name: '', value: '1' ,phone: ""}
-    ],
+    drive: [],
+    is_can_comment: 1,
+    drive_data:[],
+    drive_name : "请选择司机"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
     this.setData({
       id: options.id
     })
     wx.request({
       url: host + 'miniprogram/Admin/getDriverInfoList',
-      data:{
+      data: {
         sessionid: wx.getStorageSync("sessionid"),
       },
-      success(res)
-      {
-        var name0 = "checkboxItems[" + 0 + "].name";
-        var phone0 = "checkboxItems[" + 0 + "].phone";
-        var name1 = "checkboxItems[" + 1 + "].name";
-        var phone1 = "checkboxItems[" + 1 + "].phone";
+      success(res) {
         that.setData({
-          [name0] : res.data.data[0].name,
-          [name1]: res.data.data[1].name,
-          [phone0]: res.data.data[0].phone,
-          [phone1]: res.data.data[1].phone,
+          drive_data : res.data.data
         })
+        console.log(that.data.drive_data)
+        for(var i = 0;i < that.data.drive_data.length;i++)
+        {
+          itemList = itemList.concat(res.data.data[i].name);
+        }
       }
     })
   },
 
   //-------------选择司机事件--------------//
-  checkboxChange: function (e) {
+  open: function() {
     var that = this;
-    var length = e.detail.value.length;
-    that.setData({
-      drive: []
-    })
-    for (var i = 0; i < e.detail.value.length;i++)
-    {
-      that.setData({
-        drive: that.data.drive.concat(this.data.checkboxItems[e.detail.value[i]].phone)
-      })
-    }
-    console.log(that.data.drive)
-   
-    //-------------------------------------------------------------------//
-    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
-    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
-      checkboxItems[i].checked = false;
-
-      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-        if (checkboxItems[i].value == values[j]) {
-          checkboxItems[i].checked = true;
-          break;
+    wx.showActionSheet({
+      itemList: itemList,
+      success: function(res) {
+        if (!res.cancel) {
+          that.setData({
+            drive: that.data.drive_data[res.tapIndex].phone,
+            drive_name: that.data.drive_data[res.tapIndex].name
+          })
         }
       }
-    }
-
-    this.setData({
-      checkboxItems: checkboxItems
     });
   },
 
   //-----------------点击开关事件---------------//
-  switch_check(e)
-  {
+  switch_check(e) {
     var that = this;
-    if (e.detail.value == true)
-    {
+    if (e.detail.value == true) {
       that.setData({
-        is_can_comment : 1
+        is_can_comment: 1
       })
-    }
-    else{
+    } else {
       that.setData({
-        is_can_comment : 0
+        is_can_comment: 0
       })
     }
   },
-  post(){
+
+  //提交表单
+  post() {
     var that = this
-    if(that.data.drive.length == 0)
-    {
+    if (that.data.drive.length == 0) {
       wx.showToast({
         title: '请至少选择一个司机',
         icon: 'none',
         duration: 2000
       })
-    }
-    else
-    {
+    } else {
       wx.request({
         url: host + 'miniprogram/Admin/check',
         data: {
