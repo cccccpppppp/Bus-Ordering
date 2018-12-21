b4942e5386e5416794967b7776c39d41e71b1e5f
liuxin <1056498425@qq.com>
2018-12-21 11:27:11 +0800

    忘记取消注释了

--- pages/adminUrl/agree/agree.js
+++ pages/adminUrl/agree/agree.js
@@ -89,54 +89,53 @@ Page({
         duration: 2000
       })
     } else {
-      console.log(that.data.drive)
-      // wx.request({
-      //   url: host + 'miniprogram/Admin/check',
-      //   data: {
-      //     sessionid: wx.getStorageSync("sessionid"),
-      //     apply_id: that.data.id,
-      //     status: 1,//审核通过
-      //     driverPhonList: that.data.drive,
-      //     is_can_comment: that.data.is_can_comment
-      //   },
-      //   method : "GET",
-      //   success(res) {
-      //     console.log(res.data.status)
-      //     if (res.data.status == 0)
-      //     {
-      //       wx.showToast({
-      //         title: '已同意该订单',
-      //         icon: 'success',
-      //         duration: 2000
-      //       })
-      //       setTimeout(function () {
-      //         wx.navigateBack({
-      //           delta: 1
-      //         })
-      //       }, 2000);
-      //     }
-      //     else if(res.data.status == 1)
-      //     {
-      //       wx.showToast({
-      //         title: res.data.msg,
-      //         icon : "none",
-      //         duration : 5000
-      //       })
-      //       setTimeout(function(){
-      //         wx.navigateBack({
-      //           delta: 1 
-      //         })
-      //       },2000);
-      //     }
-      //     else{
-      //       wx.showToast({
-      //         title: res.data.msg,
-      //         icon: 'none',
-      //         duration: 5000
-      //       })
-      //     }
-      //   }
-      // })
+      wx.request({
+        url: host + 'miniprogram/Admin/check',
+        data: {
+          sessionid: wx.getStorageSync("sessionid"),
+          apply_id: that.data.id,
+          status: 1,//审核通过
+          driverPhonList: that.data.drive,
+          is_can_comment: that.data.is_can_comment
+        },
+        method : "GET",
+        success(res) {
+          console.log(res.data.status)
+          if (res.data.status == 0)
+          {
+            wx.showToast({
+              title: '已同意该订单',
+              icon: 'success',
+              duration: 2000
+            })
+            setTimeout(function () {
+              wx.navigateBack({
+                delta: 1
+              })
+            }, 2000);
+          }
+          else if(res.data.status == 1)
+          {
+            wx.showToast({
+              title: res.data.msg,
+              icon : "none",
+              duration : 5000
+            })
+            setTimeout(function(){
+              wx.navigateBack({
+                delta: 1 
+              })
+            },2000);
+          }
+          else{
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'none',
+              duration: 5000
+            })
+          }
+        }
+      })
     }
   }
 
