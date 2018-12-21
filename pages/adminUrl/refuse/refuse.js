c2844d14574750d023eb378762a6f0f6dd517e62
韦韬 <503369325@qq.com>
2018-12-21 22:33:01 +0800

    添加管理人员拒绝页面文本域计数器

--- pages/adminUrl/refuse/refuse.js
+++ pages/adminUrl/refuse/refuse.js
@@ -1,14 +1,14 @@
 const app = getApp();
-var host = app.globalData.host
+var host = app.globalData.host;
 
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     input: "",
     id: "",
+    t_text: 0
   },
 
   /**
@@ -17,43 +17,50 @@ Page({
   onLoad: function(options) {
     this.setData({
       id: options.id
-    })
+    });
   },
-  
+
   back(e) {
     var that = this;
     this.data.input = e.detail.value.input;
     wx.request({
-      url: host + 'miniprogram/Admin/check',
+      url: host + "miniprogram/Admin/check",
       data: {
         sessionid: wx.getStorageSync("sessionid"),
         apply_id: that.data.id,
         status: 2, // 审核未通过
-        fail_cause: that.data.input, //未通过原因
+        fail_cause: that.data.input //未通过原因
       },
       success(res) {
         if (res.data.msg == "成功") {
           //提示成功
           wx.showToast({
-            title: '已拒绝该申请',
-            icon: 'success',
-            duration: 3000,
+            title: "已拒绝该申请",
+            icon: "success",
+            duration: 3000
           });
           setTimeout(function() {
             wx.navigateBack({
               delta: 1
-            })
+            });
           }, 2000);
-        } 
-        else {
+        } else {
           //提示失败原因
           wx.showToast({
             title: res.data.msg,
-            icon: 'none',
+            icon: "none",
             duration: 3000
-          })
+          });
         }
       }
-    })
+    });
+  },
+  //文本域计数器
+  bindText: function(e) {
+    var t_text = e.detail.value.length;
+    // console.log(t_text)
+    this.setData({
+      counter: t_text
+    });
   }
-})
+});
