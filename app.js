8e3952b3a92583861aa2217772fe1608104eb0bc
wt <503369325@qq.com>
2019-09-10 14:36:08 +0800

    界面优化

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ App({
   globalData: {
     user_info: null,
     sessionid: null,
-    // host: "https://wx.gliet.com.cn:10002/"
+    host: "https://wx.gliet.com.cn:10002/"
     // host: "http://47.107.172.101/"
-    host: "https://my.hlj520.top/"
+    // host: "https://my.hlj520.top/"
     // host: "https://192.168.4.12/"
   }
 })
